013a9859d2925a03b7c7dadcff9cc45dac9552c7
gungorf <furkan.gungor@hepsiburada.com>
2024-11-11 19:35:10 +0300

    🔨 Add EntityId prop to the AuditLog entity

--- src/AuditSharp.Core/Entities/AuditLog.cs
+++ src/AuditSharp.Core/Entities/AuditLog.cs
@@ -1,7 +1,8 @@
 namespace AuditSharp.Core.Entities;
 
-public class AuditLog(string entityName, string operationType, string oldValues, string newValues) : AuditBase
+public class AuditLog(string entityName, string operationType, string oldValues, string newValues, string entityId) : AuditBase
 {
+    public string EntityId { get; private set; } = entityId;
     public string EntityName { get; private set; } = entityName;
     public string OperationType { get; private set; } = operationType;
     public string OldValues { get; private set; } = oldValues;
