50abdc58101ac2b4e9b64733508324506b56c942
gungorf <furkan.gungor@hepsiburada.com>
2024-11-11 19:34:53 +0300

    ♻️ Change property type from string to guid

--- src/AuditSharp.Core/Entities/AuditBase.cs
+++ src/AuditSharp.Core/Entities/AuditBase.cs
@@ -2,7 +2,7 @@ namespace AuditSharp.Core.Entities;
 
 public class AuditBase
 {
-    public required string Id { get; set; }
+    public Guid Id { get; set; }
 
     public DateTime CreationTime { get; set; }
 }
