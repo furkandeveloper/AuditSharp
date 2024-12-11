e7907c04bb2c94bbc36f81b51959435aeb952b7c
furkandeveloper <furkan.dvlp@gmail.com>
2024-12-11 16:18:05 +0300

    🔨 Implement Interceptor.cs

--- src/AuditSharp.PostgreSql/Extensions/Interceptor.cs
+++ src/AuditSharp.PostgreSql/Extensions/Interceptor.cs
@@ -1,8 +1,10 @@
 using System.Text.Json;
 using AuditSharp.Core.Entities;
+using AuditSharp.EntityFrameworkCore.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AuditSharp.PostgreSql.Extensions;
 
@@ -54,85 +56,16 @@ public class Interceptor : SaveChangesInterceptor
                 oldValues = JsonSerializer.Serialize(entry.OriginalValues.Properties.ToDictionary(p => p.Name, p => entry.OriginalValues[p]));
                 newValues = JsonSerializer.Serialize(entry.CurrentValues.Properties.ToDictionary(p => p.Name, p => entry.CurrentValues[p]));
             }
-            auditLogs.Add(new AuditLog(entry.Entity.GetType().Name, entry.State.ToString(), oldValues,newValues, GetPrimaryKeyValue(entry)));
+            auditLogs.Add(new AuditLog(entry.Entity.GetType().Name, state.ToString(), oldValues,newValues, GetPrimaryKeyValue(entry)));
         }
 
-        context.Set<AuditLog>().AddRange(auditLogs);
-
+        var auditSharpContext = IoCManager.Instance.GetRequiredService<IAuditSharpContext>();
+        foreach (var log in auditLogs)
+        {
+            auditSharpContext.InsertAsync(log);
+        }
         return base.SavedChanges(eventData, result);
     }
-    // public override int SavedChanges(SaveChangesCompletedEventData eventData, int result)
-    // {
-    //     var context = eventData.Context;
-    //     if(context == null) return base.SavedChanges(eventData, result);
-    //     var auditLogs = new List<AuditLog>();
-    //     foreach (var entry in context.ChangeTracker.Entries().Where(e => e.State == EntityState.Added || e.State == EntityState.Modified || e.State == EntityState.Deleted))
-    //     {
-    //         var newValues = string.Empty;
-    //         var oldValues = string.Empty;
-    //
-    //         if (entry.State == EntityState.Added)
-    //         {
-    //             newValues = JsonSerializer.Serialize(entry.CurrentValues.Properties.ToDictionary(p => p.Name, p => entry.CurrentValues[p]));
-    //         }
-    //         else if (entry.State == EntityState.Deleted)
-    //         {
-    //             oldValues = JsonSerializer.Serialize(entry.OriginalValues.Properties.ToDictionary(p => p.Name, p => entry.OriginalValues[p]));
-    //         }
-    //         else if (entry.State == EntityState.Modified)
-    //         {
-    //             oldValues = JsonSerializer.Serialize(entry.OriginalValues.Properties.ToDictionary(p => p.Name, p => entry.OriginalValues[p]));
-    //             newValues = JsonSerializer.Serialize(entry.CurrentValues.Properties.ToDictionary(p => p.Name, p => entry.CurrentValues[p]));
-    //         }
-    //         auditLogs.Add(new AuditLog(entry.Entity.GetType().Name, entry.State.ToString(), oldValues,newValues, GetPrimaryKeyValue(entry)));
-    //     }
-    //     context.Set<AuditLog>().AddRange(auditLogs);
-    //     return base.SavedChanges(eventData, result);
-    // }
-
-    // public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
-    // {
-    //     var context = eventData.Context;
-    //     if(context == null) return base.SavingChanges(eventData, result);
-    //     var auditLogs = new List<AuditLog>();
-    //     foreach (var entry in context.ChangeTracker.Entries().Where(e => e.State == EntityState.Added || e.State == EntityState.Modified || e.State == EntityState.Deleted))
-    //     {
-    //         var newValues = string.Empty;
-    //         var oldValues = string.Empty;
-    //
-    //         if (entry.State == EntityState.Added)
-    //         {
-    //             newValues = JsonSerializer.Serialize(entry.CurrentValues.Properties.ToDictionary(p => p.Name, p => entry.CurrentValues[p]));
-    //         }
-    //         else if (entry.State == EntityState.Deleted)
-    //         {
-    //             oldValues = JsonSerializer.Serialize(entry.OriginalValues.Properties.ToDictionary(p => p.Name, p => entry.OriginalValues[p]));
-    //         }
-    //         else if (entry.State == EntityState.Modified)
-    //         {
-    //             oldValues = JsonSerializer.Serialize(entry.OriginalValues.Properties.ToDictionary(p => p.Name, p => entry.OriginalValues[p]));
-    //             newValues = JsonSerializer.Serialize(entry.CurrentValues.Properties.ToDictionary(p => p.Name, p => entry.CurrentValues[p]));
-    //         }
-    //         auditLogs.Add(new AuditLog(entry.Entity.GetType().Name, entry.State.ToString(), oldValues,newValues, GetPrimaryKeyValue(entry)));
-    //     }
-    //     context.Set<AuditLog>().AddRange(auditLogs);
-    //     return base.SavingChanges(eventData, result);
-    // }
-
-    // public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
-    //     CancellationToken cancellationToken = new CancellationToken())
-    // {
-    //     SavingChanges(eventData, result);
-    //     return base.SavingChangesAsync(eventData, result, cancellationToken);
-    // }
-
-    // public override ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, int result,
-    //     CancellationToken cancellationToken = new CancellationToken())
-    // {
-    //     SavedChanges(eventData, result);
-    //     return base.SavedChangesAsync(eventData, result, cancellationToken);
-    // }
-
     private static string GetPrimaryKeyValue(EntityEntry entry)
     {
         var primaryKey = entry.Metadata.FindPrimaryKey();
